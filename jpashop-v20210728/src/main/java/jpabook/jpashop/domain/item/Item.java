717c6cf3f1e91f677a84ae34aa9d0c789d2ddf1e
jun9898 <nice1998@gmail.com>
2023-08-02 00:15:44 +0900

    스프링 부트와 JPA 활용1 섹션 5까지 수강 완료!

--- jpashop-v20210728/src/main/java/jpabook/jpashop/domain/item/Item.java
+++ jpashop-v20210728/src/main/java/jpabook/jpashop/domain/item/Item.java
@@ -1,11 +1,12 @@
 package jpabook.jpashop.domain.item;
 
-import jakarta.persistence.*;
 import jpabook.jpashop.domain.Category;
 import jpabook.jpashop.exception.NotEnoughStockException;
 import lombok.Getter;
 import lombok.Setter;
+import org.hibernate.annotations.BatchSize;
 
+import javax.persistence.*;
 import java.util.ArrayList;
 import java.util.List;
 
@@ -27,12 +28,17 @@ public abstract class Item {
     @ManyToMany(mappedBy = "items")
     private List<Category> categories = new ArrayList<>();
 
-    // 비즈니스 로직
-    // stock 증가
+    //==비즈니스 로직==//
+    /**
+     * stock 증가
+     */
     public void addStock(int quantity) {
         this.stockQuantity += quantity;
     }
 
+    /**
+     * stock 감소
+     */
     public void removeStock(int quantity) {
         int restStock = this.stockQuantity - quantity;
         if (restStock < 0) {
