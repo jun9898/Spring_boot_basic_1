177b7a73ba7e278cc2dc790772d9f4f76811daf9
jun9898 <nice1998@gmail.com>
2023-08-05 00:03:33 +0900

    스프링 부트와 JPA 활용1 섹션 7수강중! 변경 감지와 병합 수강중

--- jpashop/src/main/java/jpabook/jpashop/domain/item/Book.java
+++ jpashop/src/main/java/jpabook/jpashop/domain/item/Book.java
@@ -1,6 +1,5 @@
 package jpabook.jpashop.domain.item;
 
-import jakarta.persistence.DiscriminatorColumn;
 import jakarta.persistence.DiscriminatorValue;
 import jakarta.persistence.Entity;
 import lombok.Getter;
